847fae5214a2a7e467a3d27213da16559dbb87ac
Agustinus <lagustinus211@gmail.com>
2023-06-26 14:49:55 +0700

    Pass

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
-<html lang="en">
+<?php
+// Mengatur koneksi database
+$servername = "103.219.251.244";
+$username = "lahorasm_root";
+$password = "@Lgarin211";
+$dbname = "lahorasm_root2";
 
+try {
+    // Membuat koneksi
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Membuat perintah SQL untuk dijalankan
+    $sql = "INSERT INTO trafik (id, tgl) VALUES (NULL, current_timestamp())";
+
+    // Menjalankan perintah SQL
+    $conn->query($sql);
+
+    // Menampilkan pesan berhasil
+    echo "Data berhasil ditambahkan.";
+} catch (Exception $e) {
+    // Menampilkan pesan kesalahan
+    echo "Terjadi kesalahan: " . $e->getMessage();
+}
+
+// Menutup koneksi
+$conn->close();
+?>
+
+<html lang="id">
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
